dad1d9149bba479bcebfd2b68fd3631a97ef61f6
Kavia AI <production@kavia.ai>
2025-11-21 04:24:22 +0000

    Checkpoint 1 - cgcff031e2

---
+++ taxi_booking_backend/src/main/java/com/example/demo/taxibookingbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.taxibookingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class taxibookingbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(taxibookingbackendApplication.class, args);
+	}
+
+}
